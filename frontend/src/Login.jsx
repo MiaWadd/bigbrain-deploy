5dccedd6754379cef568bb7c8ee1e27bb0b04a0a
Mia Waddington <miaw@mac.home>
2025-04-10 21:19:19 +1000

    added tailwind to login page:

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -27,11 +27,34 @@ function Login({ successJob, token }) {
     }
     return (
         <>
-            <h1>{"Login"}</h1>
-            Email: <input value={email} onChange={e => setEmail(e.target.value)} type="text" /><br />
-            Password: <input value={password} onChange={e => setPassword(e.target.value)}  type="text" /><br />
-            <button onClick={login} variant="primary">{"login"}</button>
-        </>
+        <div className="mt-10 mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+            <div>
+                <h1 className="text-xl font-large text-black dark:text-white">Login</h1>                 
+                <div className="max-w-sm mx-auto mt-5">
+                    <label htmlFor="login-email" className="text-md font-normal text-black dark:text-white">Email</label>
+                    <input
+                        id='login-email' 
+                        value={email} 
+                        onChange={e => setEmail(e.target.value)}
+                        type="email"
+                        placeholder="Enter your email"
+                        className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500"
+                    />
+                </div>
+                <div className="max-w-sm mx-auto mt-5">
+                    <label htmlFor="login-password" className="text-md font-normal text-black dark:text-white">Password</label>
+                    <input
+                        id='login-password'
+                        value={password} 
+                        onChange={e => setPassword(e.target.value)}
+                        type="password"
+                        className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500"
+                    />
+                </div>
+                <button onClick={login} type='submit' className="mt-5 px-4 py-2 w-full bg-blue-500 text-white rounded-lg hover:bg-blue-600" >{"login"}</button>
+            </div>
+        </div>
+    </>
     )
 }
 
