f47d4e110555dc20588db52121f1bd260b6c29ef
Mia Waddington <miaw@mac.home>
2025-04-11 10:17:49 +1000

    added link to login and fixed spacing

--- frontend/src/Register.jsx
+++ frontend/src/Register.jsx
@@ -38,7 +38,7 @@ function Register({ successJob, token }) {
   return (
     <>
       <div className="mt-10 mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
-        <div>
+        <div className='w-full'>
           <h1 className="text-3xl text-center font-large text-black dark:text-white">Register</h1>  
           <form onSubmit={register}>
             <div className="max-w-sm mx-auto mt-5">
