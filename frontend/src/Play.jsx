c519c482ac4e7cf2f158d8144c4cf82d2606c46c
Mia Waddington <miaw@mac.home>
2025-04-14 16:57:54 +1000

    added selected button and changes to green after timer

--- frontend/src/Play.jsx
+++ frontend/src/Play.jsx
@@ -2,22 +2,25 @@ import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
+const QUESTION_TYPES = {
+  SINGLE_CHOICE: 'single-choice',
+  MULTIPLE_CHOICE: 'multiple-choice',
+  JUDGEMENT: 'judgement',
+};
+
+// Adds countdown to top right of screen
 function CountdownTimer({ duration, onExpire }) {
   const [secondsLeft, setSecondsLeft] = useState(duration);
-  
   useEffect(() => {
     if (secondsLeft <= 0) {
       if (onExpire) onExpire();
         return;
     }
-  
     const interval = setInterval(() => {
       setSecondsLeft(prev => prev - 1);
     }, 1000);
-  
     return () => clearInterval(interval);
   }, [secondsLeft, onExpire]);
-  
   return (
 		<>
 			<div className="absolute top-20 right-4 px-6 py-3 text-black text-8xl font-extrabold z-50">
@@ -27,16 +30,20 @@ function CountdownTimer({ duration, onExpire }) {
 	);
 }
 
-
 function Play({ playerId }) {
   const navigate = useNavigate();
+	// Game varaibles 
   const [playerValid, setPlayerValid] = useState(false);
   const [gameHasStarted, setGameHasStarted] = useState(false);
+	// Question variables
   const [duration, setDuration] = useState('');
   const [image, setImage] = useState('');
   const [answers, setAnswers] = useState([]);
   const [question, setQuestion] = useState('');
   const [timesUp, setTimesUp] = useState(false);
+	const [questionType, setQuestionType] = useState('');
+	const [selectedAnswers, setSelectedAnswers] = useState([]);
+	const [correctAnswers, setCorrectAnswers] = useState([]);
 
   playerId = 130896241;
   useEffect(() => {
@@ -62,98 +69,111 @@ function Play({ playerId }) {
       if (response.data.started) {
         console.log("get question number");
         try {
-                const response = await axios.get(`http://localhost:5005/play/${playerId}/question`, {
-                });
-                console.log(response.data);
-                setGameHasStarted(true);
-                setDuration(response.data.question.duration);
-                setImage("./assets.react.svg");
-                setAnswers(response.data.question.answers);
-                setQuestion(response.data.question.question);           
-
-              } catch (err) {
-                if (err.response.data.error === "Session has not started yet") {
-                    setGameHasStarted(false);
-                }
-                console.log(err);
-                // alert(err.response.data.error);
-              }
-              
-        // Make the API call to fetch question number or anything else you need
+					const response = await axios.get(`http://localhost:5005/play/${playerId}/question`);
+					console.log(response.data);
+					setGameHasStarted(true);
+					setDuration(response.data.question.duration);
+					setImage("./assets.react.svg"); // TODO CHAGE
+					setAnswers(response.data.question.answers);
+					setQuestion(response.data.question.question);   
+					setQuestionType(response.data.question.type);        
+				} catch (err) {
+					if (err.response.data.error === "Session has not started yet") {
+							setGameHasStarted(false);
+					}
+					console.log(err);
+					// alert(err.response.data.error);
+				}
       }
-    } catch (err) {
-        console.log("invaid game go to join game");
-      console.log(err);
-      alert(err.response.data.error);
+		} catch (err) {
+			console.log("invaid game go to join game");
+			console.log(err);
+			alert(err.response.data.error);
 			navigate('/join');
-    }
+		}
   };
 
-  const handleTimeUp = async () => {
-    console.log("Time's up! Locking in answer / showing result.");
-    setTimesUp(true);
-    try {
-        const response = await axios.get(`http://localhost:5005/play/${playerId}/answer`)
-        console.log(response.data.answerIds);
-    } catch (error) {
-         console.log(error);
-    }
-    // You can lock UI, or send final answer here
-  };
+	const handleTimeUp = async () => {
+		console.log("Time's up! Locking in answer / showing result.");
+		setTimesUp(true);
+		try {
+			const response = await axios.get(`http://localhost:5005/play/${playerId}/answer`);
+			console.log(response.data.answerIds);
+			setCorrectAnswers(response.data.answerIds);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  const handleAnswerClick = async (selectedAnswer) => {
-    console.log("Selected answer:", selectedAnswer);
-    // Send answer to server here using axios.post(...)
-    try {
-        const response = await axios.put(`http://localhost:5005/play/${playerId}/answer`, {
-            answers: [ "answer 1" ],
-        });
-        console.log(response);
-    } catch (error) {
-         console.log(error);
-    }
-
-  };
+	const handleAnswerClick = async (selectedAnswer) => {
+		let updatedSelection = [];
+		// If it is  single choice or judegment question, change the selected answer by either removing an already selected or selecting a new one
+		if (questionType === QUESTION_TYPES.SINGLE_CHOICE || questionType === QUESTION_TYPES.JUDGEMENT) {
+			if (selectedAnswers.includes(selectedAnswer)) {
+				updatedSelection = [];
+			} else {
+				updatedSelection = [selectedAnswer];
+			}
+		} else {
+			// If it is multiple choice, either add to the list of asnwers, or remove if it was already selected
+			if (selectedAnswers.includes(selectedAnswer)) {
+				updatedSelection = selectedAnswers.filter(a => a !== selectedAnswer);
+			} else {
+				updatedSelection = selectedAnswers.concat(selectedAnswer);
+			}
+		}
+		setSelectedAnswers(updatedSelection);
+		try {
+			await axios.put(`http://localhost:5005/play/${playerId}/answer`, {
+				answers: updatedSelection,
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
   return (
     <>
-    {playerValid && (
-      <>
-        {gameHasStarted ? (
-          <>
-            <CountdownTimer duration={duration} onExpire={handleTimeUp} />
-            {image && (
-                <img
-                src={image}
-                alt="Question visual"
-                className="mx-auto mt-6 max-w-xl max-h-96"
-                />
-            )}
-             <h3 className="mt-5 text-center text-black text-4xl font-bold">{question}</h3>
+    	{playerValid && (
+      	<>
+        	{gameHasStarted ? (
+          	<>
+            	<CountdownTimer duration={duration} onExpire={handleTimeUp} />
+            	{image && (	
+								<img src={image} alt="Image for question" className="mx-auto mt-6 max-w-xl max-h-96"/>
+            	)}
+             	<h3 className="mt-5 text-center text-black text-4xl font-bold">{question}</h3>
+							<div className="mt-6 flex flex-col gap-4 items-center">
+								{answers.map((answer, index) => {
+									const isSelected = selectedAnswers.includes(answer);
+									const isCorrect = correctAnswers.includes(answer);
+									let baseStyle = "text-white text-lg font-semibold px-6 py-3 rounded-xl shadow-md transition-all duration-200 w-3/4 max-w-xl";
+									let bgColor = "bg-blue-600 hover:bg-blue-700";
+									if (timesUp && isCorrect) {
+										bgColor = "bg-green-600";
+									} else if (isSelected) {
+										bgColor = "bg-blue-800";
+									}
 
-            {!timesUp && (
-                // {answers.length > 0 && (
-                    <div className="mt-6 flex flex-col gap-4 items-center">
-                {answers.map((answer, idx) => (
-                <button
-                    key={idx}
-                    onClick={() => handleAnswerClick(answer)}
-                    className="bg-blue-600 hover:bg-blue-700 text-white text-lg font-semibold px-6 py-3 rounded-xl shadow-md transition-all duration-200 w-3/4 max-w-xl"
-                >
-                    {answer}
-                </button>
-                ))}
-            </div>
-)}
-            {/* )} */}
-            
-          </>
-        ) : (
-          <p className="text-center text-2xl font-semibold mt-10">Waiting for game to start ...</p>
-        )}
-      </>
-    )}
-  </>
+									return (
+										<button
+											key={index}
+											onClick={() => !timesUp && handleAnswerClick(answer)}
+											className={`${bgColor} ${baseStyle}`}
+											disabled={timesUp}
+										>
+											{answer}
+										</button>
+									);
+								})}
+            	</div>
+						</>
+        	) : (
+          	<p className="text-center text-2xl font-semibold mt-10">Waiting for game to start ...</p>
+        	)}
+      	</>
+    	)}
+  	</>
   );
 }
 
